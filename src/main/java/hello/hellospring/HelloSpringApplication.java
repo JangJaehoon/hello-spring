ed90475af8b1dcff5f57dced334707c69764e2d3
Jang Jaehoon <jjhgwx@gmail.com>
2022-07-02 12:28:09 +0900

    first commit

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,16 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class HelloSpringApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
